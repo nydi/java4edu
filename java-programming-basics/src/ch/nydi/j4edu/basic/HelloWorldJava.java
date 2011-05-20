2687ef455ae988a097c2cce6ca70609faa17185e
Daniel Nydegger <daniel.nydegger@nydi.ch>
2011-05-20 20:32:59 +0200

    second eddition of Hello World

--- java-programming-basics/src/ch/nydi/j4edu/basic/HelloWorldJava.java
+++ java-programming-basics/src/ch/nydi/j4edu/basic/HelloWorldJava.java
@@ -3,6 +3,11 @@ package ch.nydi.j4edu.basic;
 public class HelloWorldJava {
 
 	public static void main(String[] args) {
+		// print out a string literal
 		System.out.println("Hello World in Java, what a success ;-)");
+		
+		// print out a variable
+		String hello = "Hello World cointained in String variable";
+		System.out.println(hello);
 	}
 }
